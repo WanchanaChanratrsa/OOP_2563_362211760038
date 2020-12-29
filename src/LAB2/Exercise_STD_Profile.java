12671080a1a139968f131956f15feec5ee667d2e
wanchana <bawzaza004@gmail.com>
2020-12-29 11:46:31 +0700

    Lab 2 : data typea and import data

---
+++ src/LAB2/Exercise_STD_Profile.java
@@ -0,0 +1,34 @@
+package LAB2;
+
+import java.util.Scanner;
+
+public class Exercise_STD_Profile {
+
+    public static void main(String[] args) {
+
+        Scanner scanner =new Scanner(System.in);
+
+        System.out.println("Enter an name: ");
+        String name = scanner.next();
+
+
+        System.out.println("Enter an lastname: ");
+        String lastname = scanner.next();
+
+
+        System.out.println("Enter an id: ");
+        String id = scanner.next();
+
+
+        System.out.println("Enter an age: ");
+        int age = scanner.nextInt();
+
+
+        System.out.println("Enter an email: ");
+        String email = scanner.next();
+
+
+
+
+    }
+}
